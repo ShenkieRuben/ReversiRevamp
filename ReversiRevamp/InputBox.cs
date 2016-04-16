6c1197813957b30177446087fd5c0064139bf88a
Ruben Schenkhuizen <ruben.schenkhuizen@gmail.com>
2016-04-16 14:55:46 +0200

    Initialize the Reversi program

---
+++ ReversiRevamp/InputBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReversiRevamp
+{
+    class InputBox : ReversiForm
+    {
+        private ReversiForm _parent;
+        public InputBox(Size size, ReversiForm parent)
+        {
+            Size = size;
+            _parent = parent;
+            StartPosition = FormStartPosition.CenterParent;
+            showInputBox();
+        }
+
+        private void showInputBox()
+        {
+
+            ShowDialog();
+        }
+    }
+}
---
+++ ReversiRevamp/ReversiForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ReversiRevamp
+{
+    partial class ReversiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReversiForm));
+            this.reversiBox = new System.Windows.Forms.PictureBox();
+            this.reversiMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.reversiBox)).BeginInit();
+            this.reversiMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // reversiBox
+            // 
+            this.reversiBox.Location = new System.Drawing.Point(12, 35);
+            this.reversiBox.Name = "reversiBox";
+            this.reversiBox.Size = new System.Drawing.Size(752, 565);
+            this.reversiBox.TabIndex = 0;
+            this.reversiBox.TabStop = false;
+            // 
+            // reversiMenu
+            // 
+            this.reversiMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.reversiMenu.Location = new System.Drawing.Point(0, 0);
+            this.reversiMenu.Name = "reversiMenu";
+            this.reversiMenu.Size = new System.Drawing.Size(776, 24);
+            this.reversiMenu.TabIndex = 1;
+            this.reversiMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            // 
+            // ReversiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 612);
+            this.Controls.Add(this.reversiBox);
+            this.Controls.Add(this.reversiMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.reversiMenu;
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "ReversiForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reversi";
+            ((System.ComponentModel.ISupportInitialize)(this.reversiBox)).EndInit();
+            this.reversiMenu.ResumeLayout(false);
+            this.reversiMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox reversiBox;
+        private System.Windows.Forms.MenuStrip reversiMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+    }
+}
+
